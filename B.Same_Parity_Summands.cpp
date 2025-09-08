d82a610056e27c9de52c4b77f58f46aac66c70dd
SaikatDeb12 <saikat.rcld@gmail.com>
2025-09-09 00:05:51 +0530

    old prob

---
+++ B.Same_Parity_Summands.cpp
@@ -0,0 +1,69 @@
+// Author: Saikat_deb | Created: 2025-09-08 23:44
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    if (n & 1) {
+        if (k & 1) {
+            cout << "YES\n";
+            int res = 0;
+            for (int i = 1; i <= 100; i++) {
+                int rem = n - ((k - 1) * i);
+                if ((i & 1 and rem & 1) or (i % 2 == 0 and rem % 2 == 0)) {
+                    res = i;
+                    break;
+                }
+            }
+            for (int i = 0; i < k - 1; i++)
+                cout << res << " ";
+            cout << n - ((k - 1) * res) << "\n";
+        } else {
+            cout << "NO\n";
+        }
+    } else {
+        if (k <= n / 2) {
+            cout << "YES\n";
+            int res = 0;
+            for (int i = 1; i <= 100; i++) {
+                int rem = n - ((k - 1) * i);
+                if ((i & 1 and rem & 1) or (i % 2 == 0 and rem % 2 == 0)) {
+                    res = i;
+                    break;
+                }
+            }
+            for (int i = 0; i < k - 1; i++)
+                cout << res << " ";
+            cout << n - ((k - 1) * res) << "\n";
+        } else {
+            if (k & 1) {
+                cout << "NO\n";
+            } else {
+                cout << "YES\n";
+                int res = 0;
+                for (int i = 1; i <= 100; i++) {
+                    int rem = n - ((k - 1) * i);
+                    if ((i & 1 and rem & 1) or (i % 2 == 0 and rem % 2 == 0)) {
+                        res = i;
+                        break;
+                    }
+                }
+                for (int i = 0; i < k - 1; i++)
+                    cout << res << " ";
+                cout << n - ((k - 1) * res) << "\n";
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout << fixed << setprecision(9);
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+        solve();
+    return 0;
+}
